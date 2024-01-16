1609328f5b82aacba47b8298f33716e3d0950213
EC2 Default User <151661528+kaoru-chansan@users.noreply.github.com>
2024-01-16 15:15:53 +0000

    [Finish]bookers2 投稿機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,26 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_paramas)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to index_path
   end
 
   def index
+    @books = all
   end
 
   def show
   end
+
+  private
+
+  def book_paramas
+    paramas.require(:book).parmit(:title, :body)
+  end
+
 end
