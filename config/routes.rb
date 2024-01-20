c42c86cd2f6daca1376d0fb0c880f525688c2ff6
EC2 Default User <151661528+kaoru-chansan@users.noreply.github.com>
2024-01-20 05:34:55 +0000

    bookers2　userのshow表示

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'homes/about', as: 'about'
 
   resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
