e4fc5af6dbde7fca0927a16dcdd5843a1d7a895d
EC2 Default User <151661528+kaoru-chansan@users.noreply.github.com>
2024-01-23 14:53:50 +0000

    [Finish]bookers2 フラッシュメッセージ,ヘッダーリンク

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,10 @@
 class HomesController < ApplicationController
   def top
+    flash.now[:notice] = "Welcome! You have signed up successfully."
   end
 
   def about
+    flash.now[:notice] = "Signed out successfully."
   end
 
 end
